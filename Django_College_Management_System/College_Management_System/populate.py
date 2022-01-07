e24ee670fbcccfc8e829f6a15a48352c61decd3f
sagardumore <sagardumore23.com>
2022-01-07 12:25:52 +0530

    CBV_CollegeManagement_Crud_And_Reletionship_SearchData_FakeData

--- Django_College_Management_System/College_Management_System/populate.py
+++ Django_College_Management_System/College_Management_System/populate.py
@@ -1,22 +1,36 @@
 import os
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'College_Management_System.settings')
-import django
 django.setup()
 
-from Department_Professor_Student.models import *
-
 from faker import Faker
-from random import *
+import random
+from Department_Professor_Student.models import Department,Professor,Student
 
+
+dept_list = list(Department.objects.all())
 fake = Faker()
 
-
-def populate(n):
+def generateStudentData(n):
     for i in range(n):
-        fname = fake.department_name()
-        fsub = fake.subject()
+        name = fake.name()
+        roll_no = random.randint(10,100)
+        marks = random.randint(10,100)
+        department = random.choice(dept_list)
+        department = fake.random_element(dept_list)
+        student_obj = Student.objects.create(name=name,roll_no=roll_no,marks=marks,department_id=department)
 
-        department_record = Department.objects.get_or_create(department_name=fname, subject=fsub)
 
-populate(200)
+generateStudentData(10)
+
+def generateProfessorData(n):
+    for i in range(n):
+        Professor_name = fake.name()
+        salary = random.randint(5,100)
+        l = random.randint(2,4)
+        department = fake.random_elements(dept_list,length=l,unique=True)
+        professor_obj = Professor.objects.create(Professor_name=Professor_name,salary=salary)
+        professor_obj.department_id.set(department)
+
+generateProfessorData(10)
